3b952ee718b6a1c18a12ee2c56895843e814f22b
Parmama <1laurakiss1@gmail.com>
2019-12-21 15:30:16 +0100

    Add files via upload

--- imageComparison.py
+++ imageComparison.py
@@ -3,6 +3,7 @@
 import cv2
 import numpy as np
 import glob
+import os
 
 
 
@@ -51,8 +52,13 @@ signs = ["20km/h_korlatozo", "30hm/h_korlatozo", "50hm/h_korlatozo",
 "kotelezo_kikerulesi_irany_jobbra", "kotelezo_kikerulesi_irany_balra",
 "korforgalom", "elozni_tilos_vege", "teherautoval_elozni_tilos_vege"]
 
+pathVariables = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", 
+"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", 
+"23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", 
+"36", "37", "38", "39", "40", "41", "42"]
+
 #read images
-smpImg = cv2.imread('/home/parma/Documents/Gépi látás/sample/00034.ppm')
+smpImg = cv2.imread('/home/parma/Documents/Gépi látás/reference/aasd/00002_00007.ppm')
 smpImg = cv2.resize(smpImg, (300,300))
 cv2.imshow("Sample", smpImg)
 '''compImg = cv2.imread('/home/parma/Documents/Gépi látás/reference/00003/00036_00001.ppm')
@@ -60,63 +66,76 @@ cv2.imshow("Sample", smpImg)
 srcImg = cv2.cvtColor(srcImg, cv2.COLOR_BGR2GRAY)
 compImg = cv2.cvtColor(compImg, cv2.COLOR_BGR2GRAY)'''
 
-filec=0
-goodCounter = 0
-a = 0
-notNull = 0
 
+filec=100
+percentage = 1
+i=0
 
-#Iterating in the reference folder
-for f in glob.iglob("/home/parma/Documents/Gépi látás/reference/00003/*"):
-	print(f)
-	refImg = cv2.imread(f)
-	refImg = cv2.resize(refImg, (300,300))
+while (percentage/(filec/10) < 10):
+
+	filec = 0
+	goodCounter = 0
+	a = 0
+	percentage = 0
 	
-	sift = cv2.xfeatures2d.SIFT_create()
+
+
+	#Iterating in the reference folder
+	pattern = "/home/parma/Documents/Gépi látás/reference/" + pathVariables[i]+ "/*"
 	
-	kp1, des1 = sift.detectAndCompute(smpImg, None)
-	kp2, des2 = sift.detectAndCompute(refImg, None)
-	
-	#skip image with empty descriptor
-	if (not np.any(des2)):
-		continue
-
-	index_params = dict(algorithm = 0, trees = 5)
-	search_params = dict()
-
-	flann = cv2.FlannBasedMatcher(index_params, search_params)
-
-	matches = flann.knnMatch(des1, des2, k=2)
-
-	good = []
-	for m, n in matches:
-		if m.distance < 0.6*n.distance:
-			good.append(m)
-
-	result = cv2.drawMatches(smpImg, kp1, refImg, kp2, good, None)
-	cv2.imshow("Found Matches", result)
-	
-	a = len(good)
-	print(a)
-	
-	if (a != 0):
-		notNull +=1
+	for fname in glob.glob(pattern):
+		print('file name :', fname)
 		
-	#count the passed images
-	filec+=1
-	print(filec)
-	
-	#count all the matches
-	goodCounter = a + goodCounter
-	print(goodCounter)
-	
-	#ends after 100 images
-	if (filec == 100):
-		asd = False
-		break
+		refImg = cv2.imread(fname)
+		refImg = cv2.resize(refImg, (300,300))
+		
+		sift = cv2.xfeatures2d.SIFT_create()
+				
+		kp1, des1 = sift.detectAndCompute(smpImg, None)
+		kp2, des2 = sift.detectAndCompute(refImg, None)
+		
 
-#match ratio
-print(goodCounter/notNull)
+		index_params = dict(algorithm = 0, trees = 5)
+		search_params = dict()
+
+		flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+		matches = flann.knnMatch(des1, des2, k=2)
+
+		good = []
+		for m, n in matches:
+			if m.distance < 0.6*n.distance:
+				good.append(m)
+
+		result = cv2.drawMatches(smpImg, kp1, refImg, kp2, good, None)
+		cv2.imshow("Found Matches", result)
+		
+		a = len(good)
+		percentage = a + percentage 
+		print("sum of good matches :", percentage)
+		
+			
+		#count the passed images
+		filec+=1
+		print("inspected image counter :", filec)
+		
+		#ends after 100 images
+		if (filec == 40):
+			break
+			
+
+	
+	if (i == 42):
+		break	
+	i += 1
+	
+	#match ratio
+	#match ratio
+	print(percentage/(filec/10))
+	print(signs[i-1])
+
+
+
 	
 	
 
